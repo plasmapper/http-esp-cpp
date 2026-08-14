ad5ff58e8e35e7af3fbdf8fa440368fec236c25c
plasmapper <99073058+plasmapper@users.noreply.github.com>
2026-08-14 16:17:48 +0200

    Fixed read timeout conversion to seconds

--- component/pl_http_server.cpp
+++ component/pl_http_server.cpp
@@ -107,7 +107,7 @@ esp_err_t HttpServer::Enable() {
   serverConfig.httpd.core_id = taskParameters.coreId;
   serverConfig.httpd.server_port = serverConfig.httpd.ctrl_port = serverConfig.port_secure = serverConfig.port_insecure = port;
   serverConfig.httpd.backlog_conn = serverConfig.httpd.max_open_sockets = maxNumberOfClients;
-  serverConfig.httpd.recv_wait_timeout = readTimeout * portTICK_PERIOD_MS / 1000 + 1;
+  serverConfig.httpd.recv_wait_timeout = readTimeout == portMAX_DELAY ? UINT16_MAX : readTimeout * portTICK_PERIOD_MS / 1000 + 1;
   serverConfig.httpd.uri_match_fn = httpd_uri_match_wildcard;
 
   ESP_RETURN_ON_ERROR(httpd_ssl_start(&serverHandle, &serverConfig), TAG, "start failed");
