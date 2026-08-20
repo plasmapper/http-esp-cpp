16141aded99f2d31a3009e7ff71d03250f4a8646
plasmapper <99073058+plasmapper@users.noreply.github.com>
2026-08-20 09:34:43 +0200

    Fixed HttpServer::Transaction::ReadRequestBody not enforcing an overall timeout

--- component/pl_http_server.cpp
+++ component/pl_http_server.cpp
@@ -263,20 +263,32 @@ esp_err_t HttpServer::Transaction::ReadRequestBody(void* dest, size_t size) {
   if (!size)
     return ESP_OK;
 
+  TimeOut_t xTimeOut;
+  vTaskSetTimeOutState(&xTimeOut);
+  TickType_t remainingTimeout = server.readTimeout;
+
   int res = 0;
-  if (dest) {
-    for (; size && (res = httpd_req_recv(req, (char*)dest, size)) > 0; size -= res, dest = (uint8_t*)dest + res);
-  }
-  else {
-    constexpr size_t discardBufferSize = 64;
-    char discardBuffer[discardBufferSize];
-    for (; size && (res = httpd_req_recv(req, discardBuffer, std::min(size, discardBufferSize))) > 0; size -= res);
-  }
+  do {
+    if (dest) {
+      res = httpd_req_recv(req, (char*)dest, size);
+      if (res > 0) {
+        size -= res;
+        dest = (uint8_t*)dest + res;
+      }
+    }
+    else {
+      constexpr size_t discardBufferSize = 64;
+      char discardBuffer[discardBufferSize];
+      res = httpd_req_recv(req, discardBuffer, std::min(size, discardBufferSize));
+      if (res > 0)
+        size -= res;
+    }
+  } while (size && res > 0 && xTaskCheckForTimeOut(&xTimeOut, &remainingTimeout) == pdFALSE);
 
   if (!size)
     return ESP_OK;
 
-  if (res == HTTPD_SOCK_ERR_TIMEOUT) {
+  if (res > 0 || res == HTTPD_SOCK_ERR_TIMEOUT) {
     responseWritten = true;
     httpd_resp_send_408(req);
     ESP_RETURN_ON_ERROR(ESP_ERR_TIMEOUT, TAG, "timeout");
